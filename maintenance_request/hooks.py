a0b7c2576d136292fa37d1758340325e623c1cc7
High Speed IT <info@gigapos.us>
2026-06-15 10:37:40 +0300

    chore: update app email to info@gigapos.us

--- maintenance_request/hooks.py
+++ maintenance_request/hooks.py
@@ -2,7 +2,7 @@ app_name = "maintenance_request"
 app_title = "Maintenance Request"
 app_publisher = "HIGH SPEED IT"
 app_description = "Device Maintenance Request Management System"
-app_email = "fuahid@gmail.com"
+app_email = "info@gigapos.us"
 app_license = "mit"
 
 # Includes in <head>
